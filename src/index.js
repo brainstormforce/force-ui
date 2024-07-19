09131340e5de2d065f99007ad15a3e750d16150e
Vrunda Kansara <vrundak@bsf.io>
2024-07-19 15:37:03 +0530

    Re testing the dynamic tailwind config

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-export { default as Button } from './index.jsx'
-export { default as withForceUI } from './utility/withForceUI.js';
+export { default as Button } from './exports.jsx'
+// export { default as withForceUI } from './utility/withForceUI.js';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,65 @@
-import {withForceUI} from "./src/index.js";
+// import {withForceUI} from "./src/index.js";
 
-const config = withForceUI({
-  content: [
-    "./src/**/*.{js,jsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-          primary: {
-            100: "#f7f3f7",
-            200: "#f0e6f0",
-            300: "#e9d9e9",
-            400: "#dcbfd9",
-            500: "#cfb5cf",
-            600: "#bfa5bf",
-            700: "#9e849e",
-            800: "#7d637d",
-            900: "#6d536d",
-          },
-        }
+// // const merge = require('lodash.merge');
+// // const libraryTailwindConfig = require('../surerank/tailwind.config.js');
+
+// // console.log();
+
+// const config = withForceUI({
+//   content: [
+//     "./src/**/*.{js,jsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//           primary: {
+//             100: "#f7f3f7",
+//             200: "#f0e6f0",
+//             300: "#e9d9e9",
+//             400: "#dcbfd9",
+//             500: "#cfb5cf",
+//             600: "#bfa5bf",
+//             700: "#9e849e",
+//             800: "#7d637d",
+//             900: "#6d536d",
+//           },
+//         }
+//     },
+//   },
+//   plugins: [],
+//   corePlugins: {
+//     preflight: false,
+//   }
+// });
+
+// export default config;
+
+const merge = require('lodash.merge');
+const customConfig = require('../surerank/tailwind.config.js');
+
+module.exports = merge({}, {
+    content: [
+      "./src/**/*.{js,jsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+            primary: {
+              100: "#f7f3f7",
+              200: "#f0e6f0",
+              300: "#e9d9e9",
+              400: "#dcbfd9",
+              500: "#cfb5cf",
+              600: "#bfa5bf",
+              700: "#9e849e",
+              800: "#7d637d",
+              900: "#6d536d",
+            },
+          }
+      },
     },
-  },
-  plugins: [],
-  corePlugins: {
-    preflight: false,
-  }
-});
-
-export default config;
+    plugins: [],
+    corePlugins: {
+      preflight: false,
+    }
+  }, customConfig);
