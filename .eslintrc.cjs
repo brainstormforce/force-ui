1a3fe0d83ef15e8ea2f696e9b9f20a64cc1b752e
rahulvarma722 <rahulvarma722@gmail.com>
2024-07-25 18:35:40 +0530

    clean up

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,7 @@
 /* eslint-env node */
 module.exports = {
-    // extends: ['eslint:recommended'],
-    // parser: '@typescript-eslint/parser',
-    // plugins: ['@typescript-eslint'],
     root: true,
     rules: {
-        // '@typescript-eslint/no-explicit-any': 'off',
         indent: 'off',
         'no-console': 'error',
     },
