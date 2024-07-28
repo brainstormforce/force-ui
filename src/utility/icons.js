911e501bf8422918c02202e97ac2a108ac434498
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2024-07-28 16:37:30 +0600

    Added minus and check mark icon

--- src/utility/icons.js
+++ src/utility/icons.js
@@ -87,4 +87,29 @@ const icons = {
   ),
 };
 
+export const CheckMarkIcon = (props) => (
+		<svg
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+      {...props}
+		>
+			<path
+				d="M20 6L9 17L4 12"
+				stroke="currentColor"
+				strokeWidth="1.4"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</svg>
+)
+
+export const MinusIcon = (props) => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M5 12H19" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round"/>
+  </svg>
+)
+
 export default icons;
