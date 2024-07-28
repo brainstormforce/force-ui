c4c0fca0d8cb1cbeb3e05837a2023942e401e696
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2024-07-28 14:01:17 +0600

    chore: Added doc comment

--- src/utility/utils.js
+++ src/utility/utils.js
@@ -5,4 +5,9 @@ export function prefix() {
 	return 'force-ui-';
 }
 
+/**
+ * For Conditional classNames and merging TailwindCSS classes.
+ * @param  {...any} classNames 
+ * @returns  {string}
+ */
 export const cn = (...classNames) => twMerge(clsx(...classNames));
