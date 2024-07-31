6ae4ef3a9a412cc20afbf3706a8e059133fd6dcd
Vrunda Kansara <vrundak@bsf.io>
2024-07-31 11:08:06 +0530

    Resolved conflicts

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
 export { default as Button } from './button';
 // export { default as Container } from "./Container";
 export { default as Switch } from './switch';
-export { default as TextArea } from "./textarea";
+export { default as TextArea } from './textarea';
 export { default as Checkbox } from './checkbox';
 export { default as RadioButton } from './radio-button';
-export { default as Badge } from "./badge";
-export { default as Input } from "./input";
+export { default as Badge } from './badge';
+export { default as Input } from './input';
 
 // export { default as Label } from "./Label";
 // import { InfoCard, InfoCardContent } from "./InfoCard";
