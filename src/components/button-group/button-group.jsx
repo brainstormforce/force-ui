0000ff798284c8829c8e220f4fe93d5a98b97bfb
Rahul Verma <rahulvarma722@gmail.com>
2024-08-06 12:12:10 +0530

    linting fixed

--- src/components/button-group/button-group.jsx
+++ src/components/button-group/button-group.jsx
@@ -1,5 +1,5 @@
-import React, { useCallback, forwardRef, isValidElement, createContext, useContext } from "react";
-import { twMerge } from "tailwind-merge";
+import React, { useCallback, forwardRef, isValidElement, createContext, useContext } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 /**
  * Context for managing the ButtonGroup state.
@@ -10,7 +10,7 @@ const ButtonGroupContext = createContext();
  * Hook to use the ButtonGroup context.
  * @return {Object} The context value of the ButtonGroup.
  */
-const useButtonGroup = () => useContext(ButtonGroupContext);
+const useButtonGroup = () => useContext( ButtonGroupContext );
 
 /**
  * ButtonGroup component to wrap Button components.
@@ -22,117 +22,117 @@ const useButtonGroup = () => useContext(ButtonGroupContext);
  * @param {string}          [props.size='md']           - Size of the buttons in the group ('xs', 'sm', 'md').
  * @param {string}          [props.iconPosition='left'] - Position of the icon in the button ('left' or 'right').
  */
-const ButtonGroup = (props) => {
-    const { children, activeItem = null, onChange, className, size = "md", iconPosition = "left" } = props;
+const ButtonGroup = ( props ) => {
+	const { children, activeItem = null, onChange, className, size = 'md', iconPosition = 'left' } = props;
 
-    const handleChange = useCallback(
-        (event, value) => {
-            if (onChange) {
-                onChange({ event, value });
-            }
-        },
-        [onChange],
-    );
+	const handleChange = useCallback(
+		( event, value ) => {
+			if ( onChange ) {
+				onChange( { event, value } );
+			}
+		},
+		[ onChange ],
+	);
 
-    const groupClassName = twMerge("box-border flex border border-border-subtle border-solid rounded", className);
+	const groupClassName = twMerge( 'box-border flex border border-border-subtle border-solid rounded', className );
 
-    return (
-        <div className={groupClassName}>
-            <ButtonGroupContext.Provider
-                value={{
-                    activeItem,
-                    onChange: handleChange,
-                    size,
-                    iconPosition,
-                }}
-            >
-                {React.Children.map(children, (child, index) => {
-                    if (!isValidElement(child)) {
-                        return null;
-                    }
-                    const isFirstChild = index === 0;
-                    const isLastChild = index === React.Children.count(children) - 1;
-                    return React.cloneElement(child, { index, isFirstChild, isLastChild });
-                })}
-            </ButtonGroupContext.Provider>
-        </div>
-    );
+	return (
+		<div className={ groupClassName }>
+			<ButtonGroupContext.Provider
+				value={ {
+					activeItem,
+					onChange: handleChange,
+					size,
+					iconPosition,
+				} }
+			>
+				{ React.Children.map( children, ( child, index ) => {
+					if ( ! isValidElement( child ) ) {
+						return null;
+					}
+					const isFirstChild = index === 0;
+					const isLastChild = index === React.Children.count( children ) - 1;
+					return React.cloneElement( child, { index, isFirstChild, isLastChild } );
+				} ) }
+			</ButtonGroupContext.Provider>
+		</div>
+	);
 };
 
 /**
  * Button component to be used within a ButtonGroup.
- * @param {Object}          props              - The properties passed to the component.
- * @param {string}          props.slug         - Unique identifier for the button.
- * @param {string}          props.text         - Text to be displayed inside the button.
- * @param {React.ReactNode} [props.icon]       - Optional icon to be displayed inside the button.
- * @param {string}          [props.className]  - Additional class names for styling.
- * @param {boolean}         props.isFirstChild - Flag indicating if this button is the first child in the group.
- * @param {boolean}         props.isLastChild  - Flag indicating if this button is the last child in the group.
+ * @param {Object}          props                  - The properties passed to the component.
+ * @param {string}          props.slug             - Unique identifier for the button.
+ * @param {string}          props.text             - Text to be displayed inside the button.
+ * @param {React.ReactNode} [props.icon]           - Optional icon to be displayed inside the button.
+ * @param {string}          [props.className]      - Additional class names for styling.
+ * @param {boolean}         props.isFirstChild     - Flag indicating if this button is the first child in the group.
+ * @param {boolean}         props.isLastChild      - Flag indicating if this button is the last child in the group.
  * @param {boolean}         [props.disabled=false] - Flag indicating if the button is disabled.
- * @param {Object}          [props.rest]       - Other properties to be passed to the button element.
- * @param {React.Ref}       ref                - Reference to the button element.
+ * @param {Object}          [props.rest]           - Other properties to be passed to the button element.
+ * @param {React.Ref}       ref                    - Reference to the button element.
  */
-const Button = (props, ref) => {
-    const providerValue = useButtonGroup();
-    const { slug, text, icon, className, disabled = false, isFirstChild, isLastChild, ...rest } = props;
+const Button = ( props, ref ) => {
+	const providerValue = useButtonGroup();
+	const { slug, text, icon, className, disabled = false, isFirstChild, isLastChild, ...rest } = props;
 
-    if (!providerValue) {
-        throw new Error("Button should be used inside Button Group");
-    }
+	if ( ! providerValue ) {
+		throw new Error( 'Button should be used inside Button Group' );
+	}
 
-    const { activeItem, onChange, size, iconPosition } = providerValue;
+	const { activeItem, onChange, size, iconPosition } = providerValue;
 
-    const sizes = {
-        xs: "py-1 px-1 text-sm gap-0.5 [&>svg]:h-4 [&>svg]:w-4",
-        sm: "py-2 px-2 text-base gap-1 [&>svg]:h-4 [&>svg]:w-4",
-        md: "py-2.5 px-2.5 text-base gap-1 [&>svg]:h-5 [&>svg]:w-5",
-    };
+	const sizes = {
+		xs: 'py-1 px-1 text-sm gap-0.5 [&>svg]:h-4 [&>svg]:w-4',
+		sm: 'py-2 px-2 text-base gap-1 [&>svg]:h-4 [&>svg]:w-4',
+		md: 'py-2.5 px-2.5 text-base gap-1 [&>svg]:h-5 [&>svg]:w-5',
+	};
 
-    const baseClasses = "bg-background-primary text-primary cursor-pointer flex items-center justify-center";
+	const baseClasses = 'bg-background-primary text-primary cursor-pointer flex items-center justify-center';
 
-    // Button hover classes.
-    const hoverClasses = "hover:bg-button-tertiary-hover";
+	// Button hover classes.
+	const hoverClasses = 'hover:bg-button-tertiary-hover';
 
-    // Button focus classes.
-    const focusClasses = "focus:outline-none";
+	// Button focus classes.
+	const focusClasses = 'focus:outline-none';
 
-    // Button disabled classes.
-    const disabledClasses = disabled ? "text-text-disabled cursor-not-allowed" : "";
+	// Button disabled classes.
+	const disabledClasses = disabled ? 'text-text-disabled cursor-not-allowed' : '';
 
-    const firstChildClasses = isFirstChild ? "rounded-tl rounded-bl border-0 border-r border-border-subtle" : "";
-    const lastChildClasses = isLastChild ? "rounded-tr rounded-br border-0" : "";
-    const borderClasses = "border-0 border-r border-border-subtle border-solid";
-    const activeClasses = activeItem === slug ? "bg-button-disabled" : "";
+	const firstChildClasses = isFirstChild ? 'rounded-tl rounded-bl border-0 border-r border-border-subtle' : '';
+	const lastChildClasses = isLastChild ? 'rounded-tr rounded-br border-0' : '';
+	const borderClasses = 'border-0 border-r border-border-subtle border-solid';
+	const activeClasses = activeItem === slug ? 'bg-button-disabled' : '';
 
-    const buttonClassName = twMerge(
-        baseClasses,
-        hoverClasses,
-        focusClasses,
-        disabledClasses,
-        sizes[size],
-        borderClasses,
-        activeClasses,
-        firstChildClasses,
-        lastChildClasses,
-        className
-    );
+	const buttonClassName = twMerge(
+		baseClasses,
+		hoverClasses,
+		focusClasses,
+		disabledClasses,
+		sizes[ size ],
+		borderClasses,
+		activeClasses,
+		firstChildClasses,
+		lastChildClasses,
+		className
+	);
 
-    const handleClick = (event) => {
-        onChange(event, { slug, text });
-    };
+	const handleClick = ( event ) => {
+		onChange( event, { slug, text } );
+	};
 
-    return (
-        <button ref={ref} className={buttonClassName} disabled={disabled} onClick={handleClick} {...rest}>
-            {iconPosition === "left" && icon && <span className="mr-1">{icon}</span>}
-            {text}
-            {iconPosition === "right" && icon && <span className="ml-1">{icon}</span>}
-        </button>
-    );
+	return (
+		<button ref={ ref } className={ buttonClassName } disabled={ disabled } onClick={ handleClick } { ...rest }>
+			{ iconPosition === 'left' && icon && <span className="mr-1">{ icon }</span> }
+			{ text }
+			{ iconPosition === 'right' && icon && <span className="ml-1">{ icon }</span> }
+		</button>
+	);
 };
 
 const exports = {
-    Group: ButtonGroup,
-    Button: forwardRef(Button),
+	Group: ButtonGroup,
+	Button: forwardRef( Button ),
 };
 
 export default exports;
