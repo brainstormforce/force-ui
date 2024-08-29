4739f8846838460bf3d62d50851feacd0231b08f
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-08-29 15:47:10 +0200

    Resolve lint js issues

--- src/components/skeleton/skeleton.jsx
+++ src/components/skeleton/skeleton.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { cn } from '@/utilities/functions';
 
-const Skeleton = ({
-    variant = 'rectengular', // rectengular, circular
-    className,
-    ...props
- }) => {
+const Skeleton = ( {
+	variant = 'rectengular', // rectengular, circular
+	className,
+	...props
+} ) => {
+	const variantClasses = {
+		circular: 'rounded-full bg-gray-200 ',
+		rectangular: 'rounded-md bg-gray-200',
+	}?.[ variant ];
 
-    const variantClasses = {
-        circular: 'rounded-full bg-gray-200 ',
-        rectangular: 'rounded-md bg-gray-200',
-    }?.[variant];
-
-    return (
-        <div className={cn( variantClasses, 'animate-pulse', className)} { ...props }></div>
-    )
-}
+	return (
+		<div className={ cn( variantClasses, 'animate-pulse', className ) } { ...props }></div>
+	);
+};
 
 export default Skeleton;
