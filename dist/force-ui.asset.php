a8e31cd86c4d356af9d57f1b8aba755cdb0e33a4
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-09-03 10:49:06 +0200

    Add animation for varinat underline

--- dist/force-ui.asset.php
+++ dist/force-ui.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '10fd9744264d2f52646c');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '8625e6cad07736f95f4d');
--- src/components/tabs/tabs.jsx
+++ src/components/tabs/tabs.jsx
@@ -6,6 +6,7 @@ import React, {
 	useContext,
 } from 'react';
 import { cn } from '@/utilities/functions';
+import { motion } from "framer-motion"; 
 
 // Context for managing the TabsGroup state.
 const TabsGroupContext = createContext();
@@ -198,11 +199,12 @@ const Tab = ( props, ref ) => {
 			onClick={ handleClick }
 			{ ...rest }
 		>
-			{
-				activeItem === slug && variant === 'underline'
-					? <span className="absolute right-0 left-0 -bottom-px h-px bg-border-interactive"></span>
-					: ''
-			}
+            {activeItem === slug && variant === "underline" && (
+                <motion.span
+                layoutId="underline"
+                className="absolute right-0 left-0 -bottom-px h-px bg-border-interactive"
+                />
+            )}
 			<span className={ iconParentClasses }>
 				{ iconPosition === 'left' && icon && (
 					<span className="mr-1">{ icon }</span>
