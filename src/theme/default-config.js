d784f62266984f68b8f8a2886fe9b046c754c48e
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2024-09-04 13:49:18 +0600

    Updated the theme configuration
     with the staging changes

--- src/theme/default-config.js
+++ src/theme/default-config.js
@@ -1,7 +1,5 @@
 const defaultTheme = {
-	content: [
-		'node_modules/@bsf/force-ui/dist/force-ui.js',
-	],
+	content: [ 'node_modules/@bsf/force-ui/dist/force-ui.js' ],
 	theme: {
 		extend: {
 			colors: {
@@ -169,6 +167,22 @@ const defaultTheme = {
 				'toggle-on-hover': '#3B82F6',
 				'toggle-on-border': '#60A5FA',
 				'toggle-off-disabled': '#F3F4F6',
+			},
+			width: {
+				'1/7': '14.2857143%',
+				'1/8': '12.5%',
+				'1/9': '11.1111111%',
+				'1/10': '10%',
+				'1/11': '9.0909091%',
+				'1/12': '8.3333333%',
+			},
+			boxShadow: {
+				'soft-shadow-sm': '0px 6px 32px -12px rgba(149, 160, 178, 0.12)',
+				'soft-shadow': '0px 8px 32px -12px rgba(149, 160, 178, 0.16)',
+				'soft-shadow-md': '0px 10px 32px -12px rgba(149, 160, 178, 0.2)',
+				'soft-shadow-lg': '0px 12px 32px -12px rgba(149, 160, 178, 0.24)',
+				'soft-shadow-xl': '0px 16px 32px -12px rgba(149, 160, 178, 0.32)',
+				'soft-shadow-2xl': '0px 24px 64px -12px rgba(149, 160, 178, 0.32)',
 			},
 		},
 	},
