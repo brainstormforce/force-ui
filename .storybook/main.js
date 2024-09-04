dd53fb9a50541af3ce5610fd27c8fc742fff4cf6
Vrunda Kansara <vrundak@bsf.io>
2024-09-04 19:06:26 +0530

    Fixing - ReferenceError: React is not defined storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,15 @@
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: ["@storybook/addon-webpack5-compiler-swc", "@storybook/addon-onboarding", "@storybook/addon-links", "@storybook/addon-essentials", "@chromatic-com/storybook", "@storybook/addon-interactions"],
+  swc: () => ({
+    jsc: {
+      transform: {
+        react: {
+          runtime: 'automatic'
+        }
+      }
+    }
+  }),
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
