fb2d6fd78f54904468508317fcc54ec798159015
BabuTechBsf <baburaom@bsf.io>
2024-09-11 19:51:36 +0530

    added read me for setup

--- dist/force-ui.asset.php
+++ dist/force-ui.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '4473aacdc71e576266eb');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'e817c595493f0d27c0d9');
