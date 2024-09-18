874d14ec96da2adfdbafb33e393b4c2f0f0e7807
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-09-18 11:54:18 +0200

    Resolve lint js issues

--- src/components/menu-item/menu-item.stories.js
+++ src/components/menu-item/menu-item.stories.js
@@ -1,96 +1,95 @@
-import { render } from 'react-dom';
 import Menu from './menu-item.jsx';
 import { CreditCard,
-    Store, PenTool, ShoppingBag, ShoppingCart, Truck, MousePointer, ChartNoAxesColumnIncreasing, Layers, CloudUpload, Bell  } from 'lucide-react';
+	Store, PenTool, ShoppingBag, ShoppingCart, Truck, MousePointer, ChartNoAxesColumnIncreasing, Layers, CloudUpload, Bell } from 'lucide-react';
 
 export default {
-    title: 'Molecules/Menu Item',
-    component: Menu,
-    parameters: {
+	title: 'Molecules/Menu Item',
+	component: Menu,
+	parameters: {
 		layout: 'centered',
 	},
-    tags: [ 'autodocs' ],
-    argTypes: {
-    size: {
-      description: 'Specifies the size of the MenuItem component',
-      control: { type: 'select' },
-      options: ['sm', 'md'],
-    },
-    active: {
-      description: 'Determines if the MenuItem is in an active state',
-      control: { type: 'boolean' },
-    },
-    disabled: {
-      description: 'Disables the MenuItem if set to true',
-      control: { type: 'boolean' },
-    },
-    onClick: {
-      action: 'clicked',
-      description: 'Callback when MenuItem is clicked',
-    },
-    children: {
-      description: 'Content inside the MenuItem',
-      control: { type: 'text' },
-    },
-  },
-}
+	tags: [ 'autodocs' ],
+	argTypes: {
+		size: {
+			description: 'Specifies the size of the MenuItem component',
+			control: { type: 'select' },
+			options: [ 'sm', 'md' ],
+		},
+		active: {
+			description: 'Determines if the MenuItem is in an active state',
+			control: { type: 'boolean' },
+		},
+		disabled: {
+			description: 'Disables the MenuItem if set to true',
+			control: { type: 'boolean' },
+		},
+		onClick: {
+			action: 'clicked',
+			description: 'Callback when MenuItem is clicked',
+		},
+		children: {
+			description: 'Content inside the MenuItem',
+			control: { type: 'text' },
+		},
+	},
+};
 
 export const MenuItemSidebar = {
-    render: (args) => (
-        <Menu size={args.size}>
-            <Menu.List heading="Store" open={true} arrow={true}>
-                <Menu.Item>
-                    <Store />
-                    <div>Store Settings</div>
-                </Menu.Item>
-                <Menu.Item disabled>
-                    <PenTool />
-                    <div>Design & Branding</div>
-                </Menu.Item>
-            </Menu.List>
-            <Menu.List heading="Orders & Sales" open={true} arrow={true}>
-                <Menu.Item>
-                    <ShoppingBag />
-                    <div>Orders & Receipts</div>
-                </Menu.Item>
-                <Menu.Item active={true}>
-                    <ShoppingCart />
-                    <div>Abandoned Checkout</div>
-                </Menu.Item>
-                <Menu.Item>
-                    <Truck />
-                    <div>Shipping</div>
-                </Menu.Item>
-                <Menu.Item>
-                    <CreditCard />
-                    <div>Payment Processors</div>
-                </Menu.Item>
-            </Menu.List>
-            <Menu.Separator />
-            <Menu.List heading="Customers" open={true} arrow={true}>
-                <Menu.Item>
-                    <MousePointer />
-                    <div>Affiliates</div>
-                </Menu.Item>
-                <Menu.Item>
-                    <ChartNoAxesColumnIncreasing />
-                    <div>Subscriptions Saver</div>
-                </Menu.Item>
-            </Menu.List>
-            <Menu.List heading="Others" open={true} arrow={true}>
-                <Menu.Item>
-                    <Layers />
-                    <div>Data Export</div>
-                </Menu.Item>
-                <Menu.Item>
-                    <CloudUpload />
-                    <div>Connection</div>
-                </Menu.Item>
-                <Menu.Item>
-                    <Bell />
-                    <div>Notification</div>
-                </Menu.Item>
-            </Menu.List>
-        </Menu>
-    )
-}
+	render: ( args ) => (
+		<Menu size={ args.size }>
+			<Menu.List heading="Store" open={ true } arrow={ true }>
+				<Menu.Item>
+					<Store />
+					<div>Store Settings</div>
+				</Menu.Item>
+				<Menu.Item disabled>
+					<PenTool />
+					<div>Design & Branding</div>
+				</Menu.Item>
+			</Menu.List>
+			<Menu.List heading="Orders & Sales" open={ true } arrow={ true }>
+				<Menu.Item>
+					<ShoppingBag />
+					<div>Orders & Receipts</div>
+				</Menu.Item>
+				<Menu.Item active={ true }>
+					<ShoppingCart />
+					<div>Abandoned Checkout</div>
+				</Menu.Item>
+				<Menu.Item>
+					<Truck />
+					<div>Shipping</div>
+				</Menu.Item>
+				<Menu.Item>
+					<CreditCard />
+					<div>Payment Processors</div>
+				</Menu.Item>
+			</Menu.List>
+			<Menu.Separator />
+			<Menu.List heading="Customers" open={ true } arrow={ true }>
+				<Menu.Item>
+					<MousePointer />
+					<div>Affiliates</div>
+				</Menu.Item>
+				<Menu.Item>
+					<ChartNoAxesColumnIncreasing />
+					<div>Subscriptions Saver</div>
+				</Menu.Item>
+			</Menu.List>
+			<Menu.List heading="Others" open={ true } arrow={ true }>
+				<Menu.Item>
+					<Layers />
+					<div>Data Export</div>
+				</Menu.Item>
+				<Menu.Item>
+					<CloudUpload />
+					<div>Connection</div>
+				</Menu.Item>
+				<Menu.Item>
+					<Bell />
+					<div>Notification</div>
+				</Menu.Item>
+			</Menu.List>
+		</Menu>
+	),
+};
--- src/components/skeleton/skeleton.stories.js
+++ src/components/skeleton/skeleton.stories.js
@@ -1,29 +1,29 @@
 import Skeleton from './skeleton.jsx';
 
 export default {
-    title: 'Atoms/Skeleton',
-    component: Skeleton,
-    parameters: {
-        layout: 'centered',
-    },
-    tags: [ 'autodocs' ],
-    argTypes: {
+	title: 'Atoms/Skeleton',
+	component: Skeleton,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: [ 'autodocs' ],
+	argTypes: {
 		variant: {
 			name: 'Variant',
 			description: 'Defines the style variant of the skeleton.',
 		},
-        className: {
-            description: 'Allows you to pass custom classes to control the size and styles',
-            control: 'text',
-        },
+		className: {
+			description: 'Allows you to pass custom classes to control the size and styles',
+			control: 'text',
+		},
 	},
-}
+};
 
-export const Rectangular = (args) => (
-    <Skeleton variant="rectangular"/>
+export const Rectangular = () => (
+	<Skeleton variant="rectangular" />
 );
 
-export const Circular = (args) => (
-  <Skeleton variant="circular"/>
+export const Circular = () => (
+	<Skeleton variant="circular" />
 );
 
