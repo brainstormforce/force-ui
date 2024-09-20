04b8c77959c261f6b6f5138fb9549a32706fc15d
Ravindra Kele <ravindrak@bsf.io>
2024-09-20 17:59:15 +0530

    SUR - 263 - Fix in Checkbox Component

--- dist/force-ui.asset.php
+++ dist/force-ui.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '928d0470b8406aadd18a');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '3841f7c324ffa1d4415d');
