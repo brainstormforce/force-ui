41b9bcdb2f67fe3be5afd6a2aa1dc51c7269c43d
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-09-24 15:48:31 +0200

    Resolve lint js issues

--- src/components/select/select.stories.js
+++ src/components/select/select.stories.js
@@ -3,14 +3,14 @@ import Select from './select.jsx';
 import Label from '../label/label.jsx';
 
 export default {
-    title: 'Molecules/Select',
-    component: Select,
-    parameters: {
-        layout: 'centered',
-    },
-    tags: [ 'autodocs' ],
-    argTypes: {
-        size: {
+	title: 'Molecules/Select',
+	component: Select,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: [ 'autodocs' ],
+	argTypes: {
+		size: {
 			description: 'Defines the size variant of the select',
 			control: { type: 'select' },
 			options: [ 'sm', 'md', 'lg' ],
@@ -19,45 +19,45 @@ export default {
 				defaultValue: { summary: 'md' },
 			},
 		},
-        multiple: {
+		multiple: {
 			description: 'If true, it will allow multiple selection.',
-            control: 'boolean',
-            table: {
+			control: 'boolean',
+			table: {
 				type: { summary: 'boolean' },
 			},
-        },
-        combobox: {
+		},
+		combobox: {
 			description: 'If true, it will show a search box.',
-            control: 'boolean',
-            table: {
+			control: 'boolean',
+			table: {
 				type: { summary: 'boolean' },
 			},
-        },
-        id: {
+		},
+		id: {
 			description: 'The `id` attribute of the select element.',
-            table: {
+			table: {
 				type: { summary: 'string' },
 			},
-        },
-        onChange: {
+		},
+		onChange: {
 			description: 'Callback function to handle the change event.',
 			table: {
 				type: { summary: 'function' },
 			},
 		},
-        by: {
-            description: 'Used to identify the selected value when value type is an `object`. Default is `"id"`.',
+		by: {
+			description: 'Used to identify the selected value when value type is an `object`. Default is `"id"`.',
 			table: {
 				type: { summary: 'string' },
 			},
-        },
-        disabled: {
-            description: 'If true, the select will be disabled.',
-            table: {
+		},
+		disabled: {
+			description: 'If true, the select will be disabled.',
+			table: {
 				type: { summary: 'boolean' },
 			},
-        },
-    },
+		},
+	},
 };
 
 const options = [
@@ -68,52 +68,52 @@ const options = [
 	'Select Item 5',
 ];
 
-const Template = (args) => {
-  const [selected, setSelected] = useState(args.multiple ? [] : '');
+const Template = ( args ) => {
+	const [ selected, setSelected ] = useState( args.multiple ? [] : '' );
 
-  return (
-    <div style={ { width: '260px' } }>
-      <Select
-        {...args}
-        onChange={setSelected}
-        selected={selected}
-      >
-        <Select.Button label="Label" />
-        <Select.Options dropdownPortalId="surerank-dashboard">
-          {options.map((option, index) => (
-            <Select.Option key={index} value={option}>
-              {option}
-            </Select.Option>
-          ))}
-        </Select.Options>
-      </Select>
-      <Label size="sm" variant="help">Hint text can be added here.<a href="https://example.com">Link</a>.</Label>
-    </div>
-  );
+	return (
+		<div style={ { width: '260px' } }>
+			<Select
+				{ ...args }
+				onChange={ setSelected }
+				selected={ selected }
+			>
+				<Select.Button label="Label" />
+				<Select.Options dropdownPortalId="surerank-dashboard">
+					{ options.map( ( option, index ) => (
+						<Select.Option key={ index } value={ option }>
+							{ option }
+						</Select.Option>
+					) ) }
+				</Select.Options>
+			</Select>
+			<Label size="sm" variant="help">Hint text can be added here.<a href="https://example.com">Link</a>.</Label>
+		</div>
+	);
 };
 
 export const BasicSelect = ( args ) => Template( { ...args } );
 BasicSelect.args = {
-  size: 'md',
-  multiple: false,
+	size: 'md',
+	multiple: false,
 };
 
 export const Combobox = ( args ) => Template( { ...args } );
 Combobox.args = {
-  size: 'md',
-  combobox: true,
-  multiple: false,
+	size: 'md',
+	combobox: true,
+	multiple: false,
 };
 
 export const Multiselect = ( args ) => Template( { ...args } );
 Multiselect.args = {
-  size: 'md',
-  multiple: true,
+	size: 'md',
+	multiple: true,
 };
 
 export const MultiselectCombobox = ( args ) => Template( { ...args } );
 MultiselectCombobox.args = {
-  size: 'md',
-  multiple: true,
-  combobox: true,
+	size: 'md',
+	multiple: true,
+	combobox: true,
 };
