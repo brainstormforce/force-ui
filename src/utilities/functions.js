16ad6df61972e85841e378643ca348d4f59c48ca
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2024-09-25 15:45:03 +0600

    Created a helper function to execute functions
    in sequence

--- src/utilities/functions.js
+++ src/utilities/functions.js
@@ -11,3 +11,12 @@ export function prefix() {
  * @return  {string} - Merged TailwindCSS classes.
  */
 export const cn = ( ...classNames ) => twMerge( clsx( ...classNames ) );
+
+/**
+ * Call provided functions with the given arguments.
+ * @param {...Function} fns
+ * @return {Function}
+ */
+export const callAll = ( ...fns ) => {
+	return ( ...args ) => fns.forEach( fn => fn?.( ...args ) );
+}
