ffcd2de9e5da166f12902e4346bcc3e0bcac99c0
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-09-30 16:06:40 +0200

    Resolved lint js issues

--- src/components/topbar/topbar.stories.js
+++ src/components/topbar/topbar.stories.js
@@ -7,9 +7,9 @@ import Avatar from '../avatar/avatar.jsx';
 
 Topbar.displayName = 'Topbar';
 Topbar.Left.displayName = 'Topbar.Left';
-Topbar.Middle.displayName = 'Topbar.Middle'; 
-Topbar.Right.displayName = 'Topbar.Right'; 
-Topbar.Item.displayName = 'Topbar.Item'; 
+Topbar.Middle.displayName = 'Topbar.Middle';
+Topbar.Right.displayName = 'Topbar.Right';
+Topbar.Item.displayName = 'Topbar.Item';
 
 export default {
 	title: 'Organisms/Topbar',
@@ -22,55 +22,55 @@ export default {
 			options: [ '0', 'xxs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl' ],
 			table: {
 				type: { summary: 'string' },
-                defaultValue: { summary: 'lg' },
+				defaultValue: { summary: 'lg' },
 			},
 		},
-        topbarLeftGap: {
-            name: 'Topbar.Left: Gap',
-            description: 'Defines the gap between Topbar items in Topbar.Left sections',
-            control: { type: 'select' },
+		topbarLeftGap: {
+			name: 'Topbar.Left: Gap',
+			description: 'Defines the gap between Topbar items in Topbar.Left sections',
+			control: { type: 'select' },
 			options: [ '0', 'xxs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl' ],
 			table: {
 				type: { summary: 'string' },
-                defaultValue: { summary: 'sm' },
+				defaultValue: { summary: 'sm' },
 			},
-        },
-        topbarMiddleGap: {
-            name: 'Topbar.Middle: Gap',
-            description: 'Defines the gap between Topbar items in Topbar.Middle sections',
-            control: { type: 'select' },
+		},
+		topbarMiddleGap: {
+			name: 'Topbar.Middle: Gap',
+			description: 'Defines the gap between Topbar items in Topbar.Middle sections',
+			control: { type: 'select' },
 			options: [ '0', 'xxs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl' ],
 			table: {
 				type: { summary: 'string' },
-                defaultValue: { summary: 'md' },
+				defaultValue: { summary: 'md' },
 			},
-        },
-        topbarRightGap: {
-            name: 'Topbar.Right: Gap',
-            description: 'Defines the gap between Topbar items in Topbar.Right sections',
-            control: { type: 'select' },
+		},
+		topbarRightGap: {
+			name: 'Topbar.Right: Gap',
+			description: 'Defines the gap between Topbar items in Topbar.Right sections',
+			control: { type: 'select' },
 			options: [ '0', 'xxs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl' ],
 			table: {
 				type: { summary: 'string' },
-                defaultValue: { summary: 'sm' },
+				defaultValue: { summary: 'sm' },
 			},
-        },
-        topbarMiddleAlign: {
-            name: 'Topbar.Middle: Align',
-            description: 'Defines how the content inside the Middle section is aligned.',
-            control: { type: 'select' },
-            options: [ 'left', 'center', 'right' ],
+		},
+		topbarMiddleAlign: {
+			name: 'Topbar.Middle: Align',
+			description: 'Defines how the content inside the Middle section is aligned.',
+			control: { type: 'select' },
+			options: [ 'left', 'center', 'right' ],
 			table: {
 				type: { summary: 'string' },
-                defaultValue: { summary: 'center' },
+				defaultValue: { summary: 'center' },
 			},
-        },
+		},
 	},
 };
 
 export const WithMiddleAlignment = ( args ) => (
 	<Topbar gap={ args.gap }>
-		<Topbar.Left gap={args.topbarLeftGap}>
+		<Topbar.Left gap={ args.topbarLeftGap }>
 			<Topbar.Item>
 				<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path fillRule="evenodd" clipRule="evenodd" d="M12.5 24C19.1275 24 24.5 18.6273 24.5 11.9999C24.5 5.37255 19.1275 0 12.5 0C5.87259 0 0.5 5.37255 0.5 11.9999C0.5 18.6273 5.87259 24 12.5 24ZM12.5517 5.99996C11.5882 5.99996 10.2547 6.55101 9.5734 7.23073L7.7229 9.07688H16.9465L20.0307 5.99996H12.5517ZM15.4111 16.7692C14.7298 17.4489 13.3964 17.9999 12.4328 17.9999H4.95388L8.03804 14.923H17.2616L15.4111 16.7692ZM18.4089 10.6153H6.18418L5.60673 11.1923C4.23941 12.423 4.64495 13.3846 6.5598 13.3846H18.8176L19.3952 12.8076C20.7492 11.5841 20.3237 10.6153 18.4089 10.6153Z" fill="#0D7EE8" />
@@ -78,7 +78,7 @@ export const WithMiddleAlignment = ( args ) => (
 			</Topbar.Item>
 		</Topbar.Left>
 
-		<Topbar.Middle align={args.topbarMiddleAlign} gap={args.topbarMiddleGap}>
+		<Topbar.Middle align={ args.topbarMiddleAlign } gap={ args.topbarMiddleGap }>
 			<Topbar.Item>
 				<div className="flex gap-2">
 					<div>Nav Item 1</div>
@@ -97,7 +97,7 @@ export const WithMiddleAlignment = ( args ) => (
 			</Topbar.Item>
 		</Topbar.Middle>
 
-		<Topbar.Right gap={args.topbarRightGap}>
+		<Topbar.Right gap={ args.topbarRightGap }>
 			<Topbar.Item>
 				<Badge label="V 0.0 2" size="xs" variant="neutral" closable={ false } />
 			</Topbar.Item>
@@ -118,7 +118,7 @@ WithMiddleAlignment.storyName = 'Topbar with Center-Aligned Middle Section';
 
 export const WithLeftAlignment = ( args ) => (
 	<Topbar gap={ args.gap }>
-		<Topbar.Left gap={args.topbarLeftGap}>
+		<Topbar.Left gap={ args.topbarLeftGap }>
 			<Topbar.Item>
 				<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path fillRule="evenodd" clipRule="evenodd" d="M12.5 24C19.1275 24 24.5 18.6273 24.5 11.9999C24.5 5.37255 19.1275 0 12.5 0C5.87259 0 0.5 5.37255 0.5 11.9999C0.5 18.6273 5.87259 24 12.5 24ZM12.5517 5.99996C11.5882 5.99996 10.2547 6.55101 9.5734 7.23073L7.7229 9.07688H16.9465L20.0307 5.99996H12.5517ZM15.4111 16.7692C14.7298 17.4489 13.3964 17.9999 12.4328 17.9999H4.95388L8.03804 14.923H17.2616L15.4111 16.7692ZM18.4089 10.6153H6.18418L5.60673 11.1923C4.23941 12.423 4.64495 13.3846 6.5598 13.3846H18.8176L19.3952 12.8076C20.7492 11.5841 20.3237 10.6153 18.4089 10.6153Z" fill="#0D7EE8" />
@@ -126,7 +126,7 @@ export const WithLeftAlignment = ( args ) => (
 			</Topbar.Item>
 		</Topbar.Left>
 
-		<Topbar.Middle align="left" gap={args.topbarMiddleGap}>
+		<Topbar.Middle align="left" gap={ args.topbarMiddleGap }>
 			<Topbar.Item>
 				<div className="flex gap-2">
 					<div>Nav Item 1</div>
@@ -145,7 +145,7 @@ export const WithLeftAlignment = ( args ) => (
 			</Topbar.Item>
 		</Topbar.Middle>
 
-		<Topbar.Right gap={args.topbarRightGap}>
+		<Topbar.Right gap={ args.topbarRightGap }>
 			<Topbar.Item>
 				<Badge label="V 0.0 2" size="xs" variant="neutral" closable={ false } />
 			</Topbar.Item>
