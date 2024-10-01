641d6c0d886018716ab751d56d8412bc3eacefe6
Vrunda Kansara <vrundak@bsf.io>
2024-10-01 11:00:59 +0530

    Avoiding mandatory description in Radiobutton

--- dist/force-ui.asset.php
+++ dist/force-ui.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '8b50556bb9e94f18f3fd');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '0749c3e084543d39719b');
