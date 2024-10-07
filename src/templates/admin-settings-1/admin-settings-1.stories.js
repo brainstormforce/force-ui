c3ac2e324823788d8e027f1de4bf3fa863a05b84
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2024-10-07 19:09:44 +0600

    Updated spacing of the Admin Settings 1

--- src/templates/admin-settings-1/admin-settings-1.stories.js
+++ src/templates/admin-settings-1/admin-settings-1.stories.js
@@ -367,75 +367,77 @@ const Template = () => {
 							</Tabs.Group>
 						</Container.Item>
 						{/* Tab Content */}
-						<Container.Item className="flex flex-col items-start gap-2.5">
-							{/* Search Engine Preview Start */}
-							<Container
-								align="center"
-								justify="between"
-								className="w-full"
-							>
-								<Container.Item>
-									<Label>
-										<span>Search Engine Preview</span>
-										<CircleHelp
-											className="size-4"
-											strokeWidth="1.5"
-										/>
-									</Label>
-								</Container.Item>
-								<Container.Item>
-									<RadioButton.Group
-										size="xs"
-										style="tile"
-										columns="2"
-										defaultValue="desktop"
-									>
-										<RadioButton.Button value="desktop">
-											<Monitor className="size-4" />
-										</RadioButton.Button>
-										<RadioButton.Button value="mobile">
-											<Smartphone className="size-4" />
-										</RadioButton.Button>
-									</RadioButton.Group>
-								</Container.Item>
-							</Container>
-							{/* Preview */}
-							<div className="p-2 rounded-lg bg-background-secondary">
-								<div className="rounded-md border border-solid border-border-subtle bg-background-primary p-4 space-y-1.5 shadow-sm">
-									{/* Site logo, title, and URL */}
-									<div className="grid grid-cols-[1.75rem_1fr] items-center gap-3">
-										{/* Site logo */}
-										<Logo className="size-7" />
-										<div className="flex flex-col gap-0.5">
-											<span className="text-text-primary font-semibold">
-												SureRank
-											</span>
-											<div className="flex items-center justify-start gap-2">
-												<span className="text-text-secondary">
-													{/* Site URL */}
-													https://www.surerank.com/
+						<Container.Item className="flex flex-col items-start gap-2">
+							<div className="flex flex-col items-start gap-2.5">
+								{/* Search Engine Preview Start */}
+								<Container
+									align="center"
+									justify="between"
+									className="w-full"
+								>
+									<Container.Item>
+										<Label>
+											<span>Search Engine Preview</span>
+											<CircleHelp
+												className="size-4"
+												strokeWidth="1.5"
+											/>
+										</Label>
+									</Container.Item>
+									<Container.Item>
+										<RadioButton.Group
+											size="xs"
+											style="tile"
+											columns="2"
+											defaultValue="desktop"
+										>
+											<RadioButton.Button value="desktop">
+												<Monitor className="size-4" />
+											</RadioButton.Button>
+											<RadioButton.Button value="mobile">
+												<Smartphone className="size-4" />
+											</RadioButton.Button>
+										</RadioButton.Group>
+									</Container.Item>
+								</Container>
+								{/* Preview */}
+								<div className="p-2 rounded-lg bg-background-secondary">
+									<div className="rounded-md border border-solid border-border-subtle bg-background-primary p-4 space-y-1.5 shadow-sm">
+										{/* Site logo, title, and URL */}
+										<div className="grid grid-cols-[1.75rem_1fr] items-center gap-3">
+											{/* Site logo */}
+											<Logo className="size-7" />
+											<div className="flex flex-col gap-0.5">
+												<span className="text-text-primary font-semibold">
+													SureRank
 												</span>
-												<EllipsisVertical className="size-3.5 text-icon-secondary" />
+												<div className="flex items-center justify-start gap-2">
+													<span className="text-text-secondary">
+														{/* Site URL */}
+														https://www.surerank.com/
+													</span>
+													<EllipsisVertical className="size-3.5 text-icon-secondary" />
+												</div>
 											</div>
 										</div>
-									</div>
-									{/* Page title and description */}
-									<div className="space-y-1">
-										<p className="text-xl leading-8 font-normal text-field-label m-0">
-											{/* Page title */}
-											SureRank
-										</p>
-										<p className="text-sm leading-5 font-medium text-text-secondary m-0">
-											{/* Page description */}
-											Level up your web design skills with
-											our blog. Learn from experts,
-											discover the latest trends, and
-											build beautiful websites.
-										</p>
+										{/* Page title and description */}
+										<div className="space-y-1">
+											<p className="text-xl leading-8 font-normal text-field-label m-0">
+												{/* Page title */}
+												SureRank
+											</p>
+											<p className="text-sm leading-5 font-medium text-text-secondary m-0">
+												{/* Page description */}
+												Level up your web design skills with
+												our blog. Learn from experts,
+												discover the latest trends, and
+												build beautiful websites.
+											</p>
+										</div>
 									</div>
 								</div>
+								{/* Search Engine Preview End */}
 							</div>
-							{/* Search Engine Preview End */}
 						</Container.Item>
 						<Container
 							direction="column"
