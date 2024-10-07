a5f283f659947f2d224cac302eac51814dfaea65
Ravindra Kele <ravindrak@bsf.io>
2024-10-07 11:09:16 +0530

    lint fix

--- src/theme/default-config.js
+++ src/theme/default-config.js
@@ -189,7 +189,6 @@ const defaultTheme = {
 				'soft-shadow-2xl':
 					'0px 24px 64px -12px rgba(149, 160, 178, 0.32)',
 				'soft-shadow-inner': '0px 1px 1px 0px rgba(0, 0, 0, 0.051)',
-
 			},
 			fontSize: {
 				tiny: '0.625rem',
