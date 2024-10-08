f1fb5e7883832a64355c2d6ffc866e4de12c9db7
HimanshuSangale <himanshu@Himanshus-MacBook-Air.local>
2024-10-08 15:08:24 +0530

    Styling class file added

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -2,76 +2,127 @@ import * as React from "react"
 import { cn } from '@/utilities/functions';
 import { ChevronLeft, ChevronRight, DotSquareIcon, EllipsisIcon } from "lucide-react"
 import { sizeClassNames } from "./component-style";
+import { createContext } from "react";
+import { useContext } from "react";
 
-const Pagination = ({ size = 'sm', className, ...props }) => (
+const PageContext = createContext();
+const usePageContext = () => useContext(PageContext);
+
+const Pagination = ({ size = 'sm', children, className, ...props }) => (
+	<PageContext.Provider value={{ size }}>
 	<nav
 		role="navigation"
 		aria-label="pagination"
-		className={cn("flex w-full justify-center", sizeClassNames[size].general, className)}
+			className={cn("flex w-full justify-center m-0", className)}
 		{...props}
-	/>
+		>
+			{children}
+		</nav>
+	</PageContext.Provider>
 )
 Pagination.displayName = "Pagination"
 
 const PaginationContent = React.forwardRef(({ className, ...props }, ref) => (
 	<ul
 		ref={ref}
-		className={cn("w-full flex justify-center flex-row items-center gap-8", className)}
+		className={cn("m-0 w-full flex justify-center flex-row items-center gap-1", className)}
 		{...props}
 	/>
 ))
 PaginationContent.displayName = "PaginationContent"
 
-const PaginationItem = React.forwardRef(({ isActive, className, ...props }, ref) => (
-	<li ref={ref} className={cn("px-4 py-2 justify-center items-center rounded text-button-secondary hover:bg-button-tertiary-hover focus:bg-button-tertiary-hover focus:ring", isActive && 'text-button-primary bg-button', className)} {...props} />
-))
+const PaginationItem = React.forwardRef(({ isActive, className, ...props }, ref) => {
+	const { size } = usePageContext();
+	return (
+		<li
+			ref={ref}
+			tabIndex={0}
+			className={cn(
+				sizeClassNames[size].general,
+				sizeClassNames[size].hover,
+				sizeClassNames[size].focus,
+				sizeClassNames[size].text,
+				isActive && 'text-brand-500 bg-brand-background-50',
+				'cursor-pointer',
+				className
+			)} {...props} />
+	)
+})
 PaginationItem.displayName = "PaginationItem"
 
 const PaginationLink = ({
 	className,
-	isActive,
-	size = "",
+	href = "#",
 	...props
-}) => (
+}) => {
+	const { size } = usePageContext();
+
+	return (
 	<a
 		className={cn(
-			'no-underline text-inherit',
+			'no-underline outline-none focus:outline-none text-inherit flex justify-center items-center',
+			// sizeClassNames[size].dimension,
+			'aspect-square',
 			className
 		)}
 		{...props}
 	/>
-)
+	)
+}
 PaginationLink.displayName = "PaginationLink"
 
-const PaginationPrevious = ({ className, ...props }) => (
+const PaginationPrevious = ({ className, ...props }) => {
+	const { size } = usePageContext();
+
+	return (
 	<PaginationLink
 		aria-label="Go to previous page"
 		size="default"
-		className={cn("", className)}
+			className={cn(
+				sizeClassNames[size].icon,
+				// sizeClassNames[size].dimension,
+				className)}
 		{...props}
 	>
-		<ChevronLeft className="h-4 w-4" />
-		{/* <span>Previous</span> */}
+			<ChevronLeft />
 	</PaginationLink>
-)
+	)
+}
 PaginationPrevious.displayName = "PaginationPrevious"
 
-const PaginationNext = ({ className, ...props }) => (
+const PaginationNext = ({ className, ...props }) => {
+	const { size } = usePageContext();
+	return (
 	<PaginationLink
 		aria-label="Go to next page"
 		size="default"
-		className={cn("", className)}
+			className={cn(
+				sizeClassNames[size].icon,
+				// sizeClassNames[size].dimension,
+				className)}
 		{...props}
 	>
-		{/* <span>Next</span> */}
-		<ChevronRight className="h-4 w-4" />
+			<ChevronRight />
 	</PaginationLink>
-)
+	)
+}
 PaginationNext.displayName = "PaginationNext"
 
-const PaginationEllipsis = ({ className, ...props }) => (
-	<EllipsisIcon className="h-4 w-4" />
-)
+const PaginationEllipsis = ({ className, ...props }) => {
+	const { size } = usePageContext();
+
+	return (
+		<div className={cn(
+			sizeClassNames[size].icon,
+			// sizeClassNames[size].dimension,
+			'aspect-square',
+
+			"flex justify-center items-end",
+			className)} {...props}>
+			<EllipsisIcon className="justify-center self-end" />
+		</div>
+	)
+}
 PaginationEllipsis.displayName = "PaginationEllipsis"
 
 export {
