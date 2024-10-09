89d2872772c1c9d5178f28fd8865449454155e26
HimanshuSangale <himanshu@Himanshus-MacBook-Air.local>
2024-10-09 20:12:33 +0530

    Added existing button element

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -2,6 +2,7 @@ import { createContext, useContext, forwardRef } from "react";
 import { cn } from '@/utilities/functions';
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { disabledClassNames, sizeClassNames } from "./component-style";
+import Button from "../button";
 
 const PageContext = createContext();
 const usePageContext = () => useContext(PageContext);
@@ -12,7 +13,7 @@ const Pagination = ({ size = 'sm', disabled = false, children, className, ...pro
 		role="navigation"
 		aria-label="pagination"
 			className={cn(
-				"flex w-full justify-center m-0",
+				"flex w-full justify-center m-0 box-border",
 				className)}
 		{...props}
 		>
@@ -31,25 +32,25 @@ const PaginationContent = forwardRef(({ className, ...props }, ref) => (
 ))
 PaginationContent.displayName = "PaginationContent"
 
-const PaginationItem = forwardRef(({ isActive, className, ...props }, ref) => {
+const PaginationItem = forwardRef(({ className, ...props }, ref) => {
 	const { size, disabled } = usePageContext();
 	return (
 		<li
 			ref={ref}
 			tabIndex={0}
 			className={cn(
-				sizeClassNames[size].general,
-				sizeClassNames[size].text,
-				disabled ? [
+				// sizeClassNames[size].general,
+				// sizeClassNames[size].text,
+				'flex',
+				disabled && [
 					disabledClassNames.general,
 					disabledClassNames.text,
-					'cursor-not-allowed'
-				] : [
-					'hover:bg-button-tertiary-hover hover:text-button-secondary',
-					'focus:bg-button-tertiary-hover focus:ring',
-					isActive && 'text-button-primary bg-brand-background-50',
-					'cursor-pointer',
 				],
+				// : [
+				// 	'hover:bg-button-tertiary-hover hover:text-button-secondary',
+				// 		'focus:bg-button-tertiary-hover focus:ring-2 focus:ring-offset-1 focus:border-solid focus:border-border-subtle focus:ring-focus',
+				// 	'cursor-pointer',
+				// ],
 				className
 			)} {...props} />
 	)
@@ -59,19 +60,34 @@ PaginationItem.displayName = "PaginationItem"
 const PaginationLink = ({
 	className,
 	href = "#",
+	children,
+	isActive,
+	icon = null,
 	...props
 }) => {
-	const { disabled } = usePageContext();
+	const { size, disabled } = usePageContext();
 	return (
-		<a
-			aria-disabled={disabled}
-			tabIndex={disabled ? -1 : 0}
+		// <a
+		// 	aria-disabled={disabled}
+		// 	tabIndex={disabled ? -1 : 0}
+		// 	className={cn(
+		// 		'no-underline outline-none focus:outline-none focus:ring-0 text-inherit flex justify-center items-center',
+		// 		className
+		// 	)}
+		// 	{...props}
+		// />
+		<Button
+			size={size}
+			variant={'ghost'}
 			className={cn(
-				'no-underline outline-none focus:outline-none text-inherit flex justify-center items-center',
-				className
+				!disabled && isActive && 'text-button-primary bg-brand-background-50',
+				disabled && 'hover:none'
 			)}
-			{...props}
-		/>
+			disabled={disabled}
+			icon={icon}
+		>
+			{children}
+		</Button>
 	)
 }
 PaginationLink.displayName = "PaginationLink"
@@ -83,13 +99,12 @@ const PaginationPrevious = ({ className, ...props }) => {
 	<PaginationLink
 			aria-label="Go to previous page"
 			className={cn(
-				sizeClassNames[size].icon,
-				disabled && disabledClassNames.icon,
+				// sizeClassNames[size].icon,
+				// disabled && disabledClassNames.icon,
 				className)}
+			icon={<ChevronLeft />}
 		{...props}
-	>
-			<ChevronLeft />
-	</PaginationLink>
+		/>
 	)
 }
 PaginationPrevious.displayName = "PaginationPrevious"
@@ -101,13 +116,12 @@ const PaginationNext = ({ className, ...props }) => {
 	<PaginationLink
 			aria-label="Go to next page"
 			className={cn(
-				sizeClassNames[size].icon,
-				disabled && disabledClassNames.icon,
+				// sizeClassNames[size].icon,
+				// disabled && disabledClassNames.icon,
 				className)}
+			icon={<ChevronRight />}
 		{...props}
-	>
-			<ChevronRight />
-	</PaginationLink>
+		/>
 	)
 }
 PaginationNext.displayName = "PaginationNext"
@@ -116,14 +130,12 @@ const PaginationEllipsis = ({ className, ...props }) => {
 	const { size } = usePageContext();
 
 	return (
-		<div className={cn(
-			sizeClassNames[size].icon,
-			className
-		)}
+		<PaginationLink
+			className={cn(className)}
 			{...props}
 		>
 			...
-		</div>
+		</PaginationLink>
 	)
 }
 PaginationEllipsis.displayName = "PaginationEllipsis"
