a3060993fc494c6764fca312289fb48f07b2d46d
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2024-10-13 16:55:48 +0600

    Removed unused code

--- src/components/dialog/dialog.stories.tsx
+++ src/components/dialog/dialog.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj, StoryFn } from '@storybook/react';
+import { Meta, StoryFn } from '@storybook/react';
 import React, { useState } from 'react';
 import Dialog from './dialog';
 import Button from '../button';
