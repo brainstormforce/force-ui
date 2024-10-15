347cf33f5140f18f3f9dfbae351e0b3f5b93f116
Ravindra Kele <ravindrak@bsf.io>
2024-10-16 00:25:06 +0530

    Editor input

--- src/components/editor-input/editor-input-style.js
+++ src/components/editor-input/editor-input-style.js
@@ -1,17 +1,17 @@
 export const editableContentAreaCommonClassNames =
-	'w-full [&>p]:w-full [&>p]:m-0';
+	'w-full min-h-[1.625rem] [&>p]:w-full [&>p]:m-0';
 
 export const editorCommonClassNames =
-	'border border-solid focus-within:ring-2 focus-within:ring-offset-2 border-field-border hover:border-border-strong focus-within:!border-focus-border focus-within:ring-focus transition duration-150 ease-in-out box-border';
+	'border border-solid focus-within:ring-2 focus-within:ring-offset-2 border-field-border hover:border-border-strong focus-within:!border-focus-border focus-within:ring-focus transition duration-150 ease-in-out';
 
 export const editorDisabledClassNames =
 	'bg-field-secondary-background border-field-border-disabled hover:border-field-border-disabled [&_p]:text-badge-color-disabled cursor-not-allowed';
 
 export const editorInputClassNames = {
-	xs: 'py-1 px-2 pr-2 rounded [&_.editor-content>p]:text-xs [&_.editor-content>p]:font-normal  [&_.pointer-events-none]:text-xs [&_.pointer-events-none]:font-normal h-6',
-	sm: 'px-3 py-2 rounded [&_.editor-content>p]:text-xs [&_.editor-content>p]:font-medium [&_.pointer-events-none]:text-xs [&_.pointer-events-none]:font-medium h-8',
-	md: 'px-3.5 py-2.5 rounded-md [&_.editor-content>p]:text-sm [&_.editor-content>p]:font-medium [&_.pointer-events-none]:text-sm [&_.pointer-events-none]:font-medium h-10',
-	lg: 'px-4 py-3 rounded-md [&_.editor-content>p]:text-base [&_.editor-content>p]:font-medium [&_.pointer-events-none]:text-base [&_.pointer-events-none]:font-medium h-12',
+	xs: 'py-1 px-2 pr-2 rounded [&_.editor-content>p]:text-xs [&_.editor-content>p]:font-normal [&_.editor-content>p]:leading-[1.5rem]  [&_.pointer-events-none]:text-xs [&_.pointer-events-none]:font-normal [&_.pointer-events-none]:leading-[1.5rem]',
+	sm: 'px-3 py-2 rounded [&_.editor-content>p]:text-xs [&_.editor-content>p]:font-medium [&_.editor-content>p]:leading-[1.625rem] [&_.pointer-events-none]:text-xs [&_.pointer-events-none]:font-medium [&_.pointer-events-none]:leading-[1.625rem]',
+	md: 'px-3.5 py-2.5 rounded-md [&_.editor-content>p]:text-sm [&_.editor-content>p]:font-medium [&_.editor-content>p]:leading-[1.625rem] [&_.pointer-events-none]:text-sm [&_.pointer-events-none]:font-medium [&_.pointer-events-none]:leading-[1.625rem]',
+	lg: 'px-4 py-3 rounded-md [&_.editor-content>p]:text-base [&_.editor-content>p]:font-medium [&_.editor-content>p]:leading-[1.6875rem] [&_.pointer-events-none]:text-base [&_.pointer-events-none]:font-medium [&_.pointer-events-none]:leading-[1.6875rem]',
 };
 
 export const comboboxDropdownCommonClassNames =
