368a594df309457eda551e1819f860950ce74189
Ravindra Kele <ravindrak@bsf.io>
2024-10-16 15:15:58 +0530

    UAT issues related to border -  fixed it with outline and rings

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -18,7 +18,7 @@ const Button = forwardRef( ( props, ref ) => {
 	} = props;
 
 	const commonClass =
-		'border border-solid cursor-pointer transition-colors duration-300 ease-in-out text-xs font-semibold focus:ring-2 focus:ring-toggle-on focus:ring-offset-2 disabled:text-text-disabled';
+		'outline outline-1 border-none cursor-pointer transition-colors duration-300 ease-in-out text-xs font-semibold focus:ring-2 focus:ring-toggle-on focus:ring-offset-2 disabled:text-text-disabled';
 
 	const loadingClass = loading
 		? 'opacity-50 disabled:cursor-not-allowed'
@@ -26,32 +26,32 @@ const Button = forwardRef( ( props, ref ) => {
 
 	const variantClassNames = {
 		primary:
-			'text-text-on-color bg-button-primary hover:bg-button-primary-hover border-button-primary hover:border-button-primary-hover disabled:bg-button-disabled disabled:border-button-disabled',
+			'text-text-on-color bg-button-primary hover:bg-button-primary-hover outline-button-primary hover:outline-button-primary-hover disabled:bg-button-disabled disabled:outline-button-disabled',
 		secondary:
-			'text-text-on-color bg-button-secondary hover:bg-button-secondary-hover border-button-secondary hover:border-button-secondary-hover disabled:bg-button-disabled disabled:border-button-disabled',
+			'text-text-on-color bg-button-secondary hover:bg-button-secondary-hover outline-button-secondary hover:outline-button-secondary-hover disabled:bg-button-disabled disabled:outline-button-disabled',
 		outline:
-			'text-button-tertiary-color border border-border-subtle bg-button-tertiary hover:bg-button-tertiary-hover hover:border-border-subtle disabled:bg-button-tertiary disabled:border-border-disabled',
-		ghost: 'text-text-primary bg-transparent border border-transparent hover:bg-button-tertiary-hover',
-		link: 'text-link-primary bg-transparent hover:text-link-primary-hover hover:underline p-0 border-0 leading-none',
+			'text-button-tertiary-color outline-border-subtle bg-button-tertiary hover:bg-button-tertiary-hover hover:outline-border-subtle disabled:bg-button-tertiary disabled:outline-border-disabled',
+		ghost: 'text-text-primary bg-transparent outline-transparent hover:bg-button-tertiary-hover',
+		link: 'outline-none text-link-primary bg-transparent hover:text-link-primary-hover hover:underline p-0 border-0 leading-none',
 	}?.[ variant ];
 
 	const destructiveClassNames =
 		destructive && ! disabled
 			? {
 				primary:
-						'bg-button-danger hover:bg-button-danger-hover border-button-danger hover:border-button-danger-hover',
+						'bg-button-danger hover:bg-button-danger-hover outline-button-danger hover:outline-button-danger-hover',
 				outline:
-						'text-button-danger border border-button-danger hover:border-button-danger bg-button-tertiary hover:bg-field-background-error',
+						'text-button-danger outline outline-1 outline-button-danger hover:outline-button-danger bg-button-tertiary hover:bg-field-background-error',
 				ghost: 'text-button-danger hover:bg-field-background-error',
 				link: 'text-button-danger hover:text-button-danger-secondary',
 			}?.[ variant ]
 			: '';
 
 	const sizeClassNames = {
-		xs: 'p-1 rounded [&>svg]:size-4 h-6',
-		sm: 'p-2 rounded [&>svg]:size-4 h-8 gap-0.5',
-		md: 'p-2.5 rounded-md text-sm [&>svg]:size-5 h-10 gap-1',
-		lg: 'p-3 rounded-lg text-base [&>svg]:size-6 h-12 gap-1',
+		xs: 'p-1 rounded [&>svg]:size-4',
+		sm: 'p-2 rounded [&>svg]:size-4 gap-0.5',
+		md: 'p-2.5 rounded-md text-sm [&>svg]:size-5 gap-1',
+		lg: 'p-3 rounded-lg text-base [&>svg]:size-6 gap-1',
 	}?.[ size ];
 
 	let iconLeft,
