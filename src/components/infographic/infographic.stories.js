2157237ed34c89d7b6a47b1b89cdd06fd82b48a3
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-10-24 11:48:54 +0200

    Testing some react rechart charts

---
+++ src/components/infographic/infographic.stories.js
@@ -0,0 +1,62 @@
+// import LineChartComponent from './infographic.jsx';
+import AreaChartComponent from './infographic.jsx';
+// import BarChartComponent from './infographic.jsx';
+// import PieChartComponent from './infographic.jsx';
+
+
+// Line chart
+// export default {
+//     title: "Molecules/LineChartComponent",
+//     component: LineChartComponent,
+//     tags: [ 'autodocs' ],
+// };
+
+// const Template = (args) => <LineChartComponent {...args} />;
+
+// // Basic Story for Infographic
+// export const Default = Template.bind({});
+// Default.args = {};
+
+
+
+// Area chart
+export default {
+    title: "Molecules/AreaChartComponent",
+    component: AreaChartComponent,
+    tags: [ 'autodocs' ],
+};
+
+const Template = (args) => <AreaChartComponent {...args} />;
+
+// Basic Story for Infographic
+export const Default = Template.bind({});
+Default.args = {};
+
+
+// Bar chart
+// export default {
+//     title: "Molecules/BarChartComponent",
+//     component: BarChartComponent,
+//     tags: [ 'autodocs' ],
+// };
+
+// const Template = (args) => <BarChartComponent {...args} />;
+
+// // Basic Story for Infographic
+// export const Default = Template.bind({});
+// Default.args = {};
+
+
+
+// Pie chart
+// export default {
+//     title: "Molecules/PieChartComponent",
+//     component: PieChartComponent,
+//     tags: [ 'autodocs' ],
+// };
+
+// const Template = (args) => <PieChartComponent {...args} />;
+
+// // Basic Story for Infographic
+// export const Default = Template.bind({});
+// Default.args = {};
