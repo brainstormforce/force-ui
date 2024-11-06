abe9e63e8b4060a061c4b7f1835ba12e954f87e0
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2024-11-06 09:28:09 +0600

    Updated the gap type

--- src/components/container/container-types.ts
+++ src/components/container/container-types.ts
@@ -55,7 +55,7 @@ export interface ContainerCommonProps extends ContainerCoreCommonProps {
 export type TRange = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 export type TContainerType = 'grid' | 'flex';
 export type TCols = TRange;
-export type TGap = 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+export type TGap = 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
 export type TJustify =
 	| 'start'
 	| 'center'
