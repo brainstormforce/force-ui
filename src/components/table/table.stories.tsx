2450b0d8301a674586c513b519e5cca6209ba3e4
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2024-12-13 14:29:05 +0600

    Created story for the table component

--- src/components/table/table.stories.tsx
+++ src/components/table/table.stories.tsx
@@ -1,7 +1,8 @@
-import type { ComponentType } from 'react';
-import Table from './table';
+import { useState, type ComponentType } from 'react';
 import { StoryFn, Meta } from '@storybook/react';
-import { Pagination } from '@/components';
+import Table from './table';
+import { Button, Pagination, Tooltip } from '@/components';
+import { Edit, Trash } from 'lucide-react';
 
 const meta = {
 	title: 'Atoms/Table',
@@ -47,28 +48,70 @@ const data = [
 	},
 ];
 
-export const Default: Story = () => {
+const Template: Story = ( { checkboxSelection } ) => {
+	const [ selected, setSelected ] = useState<string[]>( [] );
+
+	const handleCheckboxChange = ( checked: boolean, value: typeof data[ number ] ) => {
+		if ( checked ) {
+			setSelected( [ ...selected, value.name ] );
+		} else {
+			setSelected( selected.filter( ( item: string ) => item !== value.name ) );
+		}
+	};
+
+	const toggleSelectAll = ( checked: boolean ) => {
+		if ( checked ) {
+			setSelected( data.map( ( item ) => item.name ) );
+		} else {
+			setSelected( [] );
+		}
+	};
+
 	return (
-		<Table>
-			<Table.Head>
+		<Table
+			checkboxSelection={ checkboxSelection }
+		>
+			<Table.Head
+				selected={ selected.length > 0 }
+				onChangeSelection={ toggleSelectAll }
+				indeterminate={ selected.length > 0 && selected.length < data.length }
+			>
 				<Table.HeadCell>Name</Table.HeadCell>
 				<Table.HeadCell>Age</Table.HeadCell>
 				<Table.HeadCell>Email</Table.HeadCell>
 				<Table.HeadCell>Phone</Table.HeadCell>
+				<Table.HeadCell></Table.HeadCell>
 			</Table.Head>
 			<Table.Body>
 				{ data.map( ( item, index ) => (
-					<Table.Row key={ index }>
+					<Table.Row
+						key={ index }
+						value={ item }
+						selected={ selected.includes( item.name ) }
+						onChangeSelection={ handleCheckboxChange }
+					>
 						<Table.Cell>{ item.name }</Table.Cell>
 						<Table.Cell>{ item.age }</Table.Cell>
 						<Table.Cell>{ item.email }</Table.Cell>
 						<Table.Cell>{ item.phone }</Table.Cell>
+						<Table.Cell>
+							<div className="flex items-center gap-2">
+								<Tooltip content="Delete" arrow placement="top">
+									<Button variant="ghost" icon={ <Trash /> } size="xs" className="text-icon-secondary hover:text-icon-primary" />
+								</Tooltip>
+								<Tooltip content="Edit" arrow placement="top">
+									<Button variant="ghost" icon={ <Edit /> } size="xs" className="text-icon-secondary hover:text-icon-primary" />
+								</Tooltip>
+							</div>
+						</Table.Cell>
 					</Table.Row>
 				) ) }
 			</Table.Body>
 			<Table.Footer>
 				<div className="flex items-center justify-between w-full">
-					<span className="text-sm font-normal leading-5 text-text-secondary">Page 1 out of 10</span>
+					<span className="text-sm font-normal leading-5 text-text-secondary">
+						Page 1 out of 9
+					</span>
 					<Pagination className="w-fit">
 						<Pagination.Content>
 							<Pagination.Previous />
@@ -86,4 +129,130 @@ export const Default: Story = () => {
 			</Table.Footer>
 		</Table>
 	);
+};
+
+export const Default = Template.bind( {} );
+Default.args = {
+	checkboxSelection: false,
+};
+Default.parameters = {
+	docs: {
+		source: {
+			code: `
+import { useState } from 'react';
+import { Table, Button, Pagination, Tooltip } from '@bsf/force-ui';
+import { Edit, Trash } from 'lucide-react';
+
+const data = [
+	{
+		name: 'John Doe',
+		age: 30,
+		email: 'KXk7g@example.com',
+		phone: '1234567890',
+	},
+	{
+		name: 'Jane Doe',
+		age: 25,
+		email: 'oXHsO@example.com',
+		phone: '1234567890',
+	},
+	{
+		name: 'Bob Smith',
+		age: 40,
+		email: 'oXHsO@example.com',
+		phone: '1234567890',
+	},
+	{
+		name: 'Alice Johnson',
+		age: 35,
+		email: 'oXHsO@example.com',
+		phone: '1234567890',
+	},
+];
+
+const App = () => {
+	const [ selected, setSelected ] = useState<string[]>( [] );
+
+	const handleCheckboxChange = ( checked: boolean, value: typeof data[ number ] ) => {
+		if ( checked ) {
+			setSelected( [ ...selected, value.name ] );
+		} else {
+			setSelected( selected.filter( ( item: string ) => item !== value.name ) );
+		}
+	};
+
+	const toggleSelectAll = ( checked: boolean ) => {
+		if ( checked ) {
+			setSelected( data.map( ( item ) => item.name ) );
+		} else {
+			setSelected( [] );
+		}
+	};
+
+	return (
+		<Table
+			checkboxSelection={ true }
+		>
+			<Table.Head
+				selected={ selected.length > 0 }
+				onChangeSelection={ toggleSelectAll }
+				indeterminate={ selected.length > 0 && selected.length < data.length }
+			>
+				<Table.HeadCell>Name</Table.HeadCell>
+				<Table.HeadCell>Age</Table.HeadCell>
+				<Table.HeadCell>Email</Table.HeadCell>
+				<Table.HeadCell>Phone</Table.HeadCell>
+				<Table.HeadCell></Table.HeadCell>
+			</Table.Head>
+			<Table.Body>
+				{ data.map( ( item, index ) => (
+					<Table.Row
+						key={ index }
+						value={ item }
+						selected={ selected.includes( item.name ) }
+						onChangeSelection={ handleCheckboxChange }
+					>
+						<Table.Cell>{ item.name }</Table.Cell>
+						<Table.Cell>{ item.age }</Table.Cell>
+						<Table.Cell>{ item.email }</Table.Cell>
+						<Table.Cell>{ item.phone }</Table.Cell>
+						<Table.Cell>
+							<div className="flex items-center gap-2">
+								<Tooltip content="Delete" arrow placement="top">
+									<Button variant="ghost" icon={ <Trash /> } size="xs" className="text-icon-secondary hover:text-icon-primary" />
+								</Tooltip>
+								<Tooltip content="Edit" arrow placement="top">
+									<Button variant="ghost" icon={ <Edit /> } size="xs" className="text-icon-secondary hover:text-icon-primary" />
+								</Tooltip>
+							</div>
+						</Table.Cell>
+					</Table.Row>
+				) ) }
+			</Table.Body>
+			<Table.Footer>
+				<div className="flex items-center justify-between w-full">
+					<span className="text-sm font-normal leading-5 text-text-secondary">
+						Page 1 out of 9
+					</span>
+					<Pagination className="w-fit">
+						<Pagination.Content>
+							<Pagination.Previous />
+							<Pagination.Item isActive>1</Pagination.Item>
+							<Pagination.Item>2</Pagination.Item>
+							<Pagination.Item>3</Pagination.Item>
+							<Pagination.Ellipsis />
+							<Pagination.Item>7</Pagination.Item>
+							<Pagination.Item>8</Pagination.Item>
+							<Pagination.Item>9</Pagination.Item>
+							<Pagination.Next />
+						</Pagination.Content>
+					</Pagination>
+				</div>
+			</Table.Footer>
+		</Table>
+	);
+}
+			`,
+		},
+	},
 };
