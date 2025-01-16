16a87fda80ed1594ab7a59d035b89b6ecef88aaa
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2025-01-16 17:06:45 +0600

    wip: Hamburger menu component

--- src/components/topbar/hamburger-menu.tsx
+++ src/components/topbar/hamburger-menu.tsx
@@ -1,7 +1,15 @@
 import { Button } from '@/components';
 import { cn } from '@/utilities/functions';
 import { motion, useCycle } from 'framer-motion';
-import { type ReactNode, useEffect, useRef } from 'react';
+import {
+	useEffect,
+	useRef,
+	createContext,
+	useContext,
+	type RefObject,
+	type ReactNode,
+	useState,
+} from 'react';
 
 type MenuToggleFn = () => void;
 type NavigationOption = { label: ReactNode; path: string; icon: React.ElementType };
@@ -11,7 +19,7 @@ interface HamburgerMenuProps {
 }
 
 interface MenuToggleProps {
-	toggle: MenuToggleFn;
+	className?: string;
 }
 
 interface NavigationProps {
@@ -30,6 +38,18 @@ interface PathProps extends React.SVGProps<SVGPathElement> {
 	transition?: Record<string, string | number>;
 }
 
+interface HamBurgerContextType {
+	isOpen?: boolean;
+	toggleOpen?: MenuToggleFn;
+	setTriggerRef?: ( ref: HTMLButtonElement ) => void;
+	triggerRef?: HTMLButtonElement;
+}
+
+// Hamburger context
+const HamBurgerContext = createContext<HamBurgerContextType>( {} );
+const HamBurgerProvider = HamBurgerContext.Provider;
+const useHamBurgerState = () => useContext( HamBurgerContext );
+
 export const useDimensions = ( ref: React.RefObject<HTMLElement> ) => {
 	const dimensions = useRef( { width: 0, height: 0 } );
 
@@ -45,24 +65,34 @@ export const useDimensions = ( ref: React.RefObject<HTMLElement> ) => {
 	return dimensions.current;
 };
 
-const sidebar = {
-	open: ( height: number = 1000 ) => ( {
-		clipPath: `circle(${ ( height * 2 ) + 200 }px at 14px 14px)`,
-		transition: {
-			type: 'spring',
-			stiffness: 20,
-			restDelta: 2,
+const sidebar = ( triggerButton: RefObject<HTMLButtonElement> ) => {
+	// Calculate the position of the trigger button
+	const buttonData = triggerButton?.getBoundingClientRect();
+	const buttonX = ( buttonData?.x ?? 0 ) + ( ( buttonData?.width ?? 0 ) / 2 );
+	const buttonY = ( buttonData?.y ?? 0 ) + ( ( buttonData?.height ?? 0 ) / 2 );
+	const buttonArea = ( buttonData?.width ?? 0 ) / 2;
+
+	console.log( buttonX, buttonY, buttonArea );
+
+	return {
+		open: ( height: number = 1000 ) => ( {
+			clipPath: `circle(${ ( height * 2 ) + 200 }px at ${ buttonX }px ${ buttonY }px)`,
+			transition: {
+				type: 'spring',
+				stiffness: 20,
+				restDelta: 2,
+			},
+		} ),
+		closed: {
+			clipPath: `circle(${ buttonArea }px at ${ buttonX }px ${ buttonY }px)`,
+			transition: {
+				delay: 0.5,
+				type: 'spring',
+				stiffness: 400,
+				damping: 40,
+			},
 		},
-	} ),
-	closed: {
-		clipPath: 'circle(12px at 31.5px 32.5px)',
-		transition: {
-			delay: 0.5,
-			type: 'spring',
-			stiffness: 400,
-			damping: 40,
-		},
-	},
+	};
 };
 
 const Path = ( props: PathProps ): React.ReactNode => (
@@ -76,50 +106,57 @@ const Path = ( props: PathProps ): React.ReactNode => (
 	/>
 );
 
-export const MenuToggle = ( { toggle }: MenuToggleProps ) => (
-	<Button
-		className="absolute top-5 left-5 rounded-full hover:shadow-sm focus:[box-shadow:none] pointer-events-auto"
-		variant="ghost"
-		size="xs"
-		onClick={ toggle }
-		icon={
-			<motion.svg
-				className="shrink-0 stroke-icon-primary"
-				width="23"
-				height="23"
-				variants={ {
-					open: {
-						viewBox: '0 0 20 20',
-					},
-					closed: {
-						viewBox: '0 0 23 18',
-					},
-				} }
-			>
-				<Path
+export const MenuToggle = ( { className }: MenuToggleProps ) => {
+	const { toggleOpen, setTriggerRef } = useHamBurgerState();
+
+	return (
+		<Button
+			// @ts-expect-error Ref is not present in Button component type, but we need it for the hamburger menu
+			ref={ setTriggerRef }
+			// className="absolute top-5 left-5 rounded-full hover:shadow-sm focus:[box-shadow:none] pointer-events-auto"
+			className={ cn( 'relative z-[1] rounded-full hover:shadow-sm focus:[box-shadow:none] pointer-events-auto', className ) }
+			variant="ghost"
+			size="xs"
+			onClick={ toggleOpen }
+			icon={
+				<motion.svg
+					className="shrink-0 stroke-icon-primary"
+					width="23"
+					height="23"
 					variants={ {
-						closed: { d: 'M 2 2.5 L 20 2.5' },
-						open: { d: 'M 3 16.5 L 17 2.5' },
+						open: {
+							viewBox: '0 0 20 20',
+						},
+						closed: {
+							viewBox: '0 0 23 18',
+						},
 					} }
-				/>
-				<Path
-					d="M 2 9.423 L 20 9.423"
-					variants={ {
-						closed: { opacity: 1 },
-						open: { opacity: 0 },
-					} }
-					transition={ { duration: 0.1 } }
-				/>
-				<Path
-					variants={ {
-						closed: { d: 'M 2 16.346 L 20 16.346' },
-						open: { d: 'M 3 2.5 L 17 16.346' },
-					} }
-				/>
-			</motion.svg>
-		}
-	/>
-);
+				>
+					<Path
+						variants={ {
+							closed: { d: 'M 2 2.5 L 20 2.5' },
+							open: { d: 'M 3 16.5 L 17 2.5' },
+						} }
+					/>
+					<Path
+						d="M 2 9.423 L 20 9.423"
+						variants={ {
+							closed: { opacity: 1 },
+							open: { opacity: 0 },
+						} }
+						transition={ { duration: 0.1 } }
+					/>
+					<Path
+						variants={ {
+							closed: { d: 'M 2 16.346 L 20 16.346' },
+							open: { d: 'M 3 2.5 L 17 16.346' },
+						} }
+					/>
+				</motion.svg>
+			}
+		/>
+	);
+};
 
 const navVariants = {
 	open: {
@@ -192,7 +229,7 @@ export const MenuItem = ( { children }: MenuItemProps ) => {
 		<motion.li
 			className="m-0 p-0 flex items-center justify-start w-full"
 			variants={ menuItemVariants }
-			whileHover={ { scale: 1.1 } }
+			whileHover={ { scale: 1.05 } }
 			whileTap={ { scale: 0.95 } }
 		>
 			{ children }
@@ -200,36 +237,61 @@ export const MenuItem = ( { children }: MenuItemProps ) => {
 	);
 };
 
+const MenuOptions = ( { options }: MenuOptionsProps ) => {
+	const { triggerRef } = useHamBurgerState();
+	if ( ! triggerRef ) {
+		return null;
+	}
+
+	return (
+		<motion.div layoutRoot className="absolute top-0 left-0 bottom-0 w-80 h-full">
+			<motion.div
+				className="bg-background-primary shadow-lg fixed top-0 bottom-0 left-0 w-80 border-y-0 border-l-0 border-r border-solid border-border-subtle"
+				variants={ sidebar( triggerRef ) }
+				layoutId="hamburger-menu-options"
+			/>
+			<Navigation options={ options } />
+		</motion.div>
+	);
+};
+
 const HamburgerMenu = ( { options }: HamburgerMenuProps ) => {
 	const [ isOpen, toggleOpen ] = useCycle( false, true );
+	const [ triggerRef, setTriggerRef ] = useState( null );
 	const containerRef = useRef( null );
 	const { height } = useDimensions( containerRef );
 
+	const providerValue = {
+		isOpen,
+		toggleOpen,
+		setTriggerRef,
+		triggerRef,
+	};
+
 	return (
-		<div className="size-6">
-			<motion.nav
-				className="absolute top-0 left-0 bottom-0 w-80 max-[782px]:h-[calc(100dvh_-_46px)] h-[calc(100dvh_-_32px)]"
-				initial={ false }
-				animate={ isOpen ? 'open' : 'closed' }
-				custom={ height }
-				variants={ {
-					open: {
-						pointerEvents: 'auto',
-					},
-					closed: {
-						pointerEvents: 'none',
-					},
-				} }
-				ref={ containerRef }
-			>
-				<motion.div
-					className="bg-background-primary shadow-lg absolute top-0 bottom-0 left-0 w-80 border-r border-solid border-border-subtle"
-					variants={ sidebar }
-				/>
-				<Navigation options={ options } />
-				<MenuToggle toggle={ () => toggleOpen() } />
-			</motion.nav>
-		</div>
+		<HamBurgerProvider value={ providerValue }>
+			<div className="size-6">
+				<motion.nav
+					className="w-80 h-full"
+					initial={ false }
+					animate={ isOpen ? 'open' : 'closed' }
+					custom={ height }
+					variants={ {
+						open: {
+							pointerEvents: 'auto',
+						},
+						closed: {
+							pointerEvents: 'none',
+						},
+					} }
+					ref={ containerRef }
+				>
+
+					<MenuToggle />
+					<MenuOptions options={ options } />
+				</motion.nav>
+			</div>
+		</HamBurgerProvider>
 	);
 };
 
