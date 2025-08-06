106aef889eb72d112afcf2389716a815d5fb33e2
ruz3n <ratna69420@gmail.com>
2025-08-06 21:43:56 +0545

    fixed width and padding and matched the figma

--- src/templates/onboarding-surerank-import-progress/onboarding-surerank-import-progress.stories.jsx
+++ src/templates/onboarding-surerank-import-progress/onboarding-surerank-import-progress.stories.jsx
@@ -25,8 +25,8 @@ const Template = ( args ) => {
 			{ ...args }
 			className="bg-background-secondary min-h-screen w-full pb-10"
 		>
-			<Topbar className="bg-background-secondary">
-				<Topbar.Left>
+			<Topbar className="bg-background-secondary p-0">
+				<Topbar.Left className="p-4">
 					<Topbar.Item>
 						<SureRankLogoFull />
 					</Topbar.Item>
@@ -42,7 +42,7 @@ const Template = ( args ) => {
 						</ProgressSteps>
 					</Topbar.Item>
 				</Topbar.Middle>
-				<Topbar.Right>
+				<Topbar.Right className="p-4">
 					<Topbar.Item>
 						<Button
 							icon={ <X className="size-4" /> }
@@ -55,8 +55,8 @@ const Template = ( args ) => {
 					</Topbar.Item>
 				</Topbar.Right>
 			</Topbar>
-			<div className="md:w-[45rem] box-border mx-auto p-8 mt-10 border border-solid border-border-subtle bg-background-primary rounded-xl shadow-sm">
-				<div>
+			<div className="max-w-xl box-border mx-auto p-6 mt-10 border border-solid border-border-subtle bg-background-primary rounded-xl shadow-sm space-y-2">
+				<div className="p-2 space-y-2">
 					<Title
 						size="md"
 						className="text-text-primary"
@@ -67,7 +67,7 @@ const Template = ( args ) => {
 						We found other SEO plugins installed on your website. Choose the ones you would like to import data from into SureRank.
 					</Label>
 				</div>
-				<div className="py-6">
+				<div className="p-2 space-y-3">
 					<RadioButton.Group vertical={ true }>
 						<RadioButton.Button
 							value="rankMath"
@@ -78,26 +78,27 @@ const Template = ( args ) => {
 							checked={ true }
 						/>
 					</RadioButton.Group>
-					<div className="mt-6">
-						<div className="mb-4">
-							<Text weight="semibold" size="base" className="text-text-primary">
-								Importing SEO Settings into SureRank
+				</div>
+				<div className="p-2 space-y-3">
+					<div className="px-2">
+						<Text weight="semibold" size="base" className="text-text-primary">
+							Importing SEO Settings into SureRank
+						</Text>
+					</div>
+					<div className="px-2 space-y-2">
+						<ProgressBar progress={ 50 } className="h-2" />
+						<div className="flex justify-between items-center">
+							<Text className="text-field-helper text-xs font-normal">
+								Importing SEO page titles......
 							</Text>
-						</div>
-						<div className="space-y-2">
-							<ProgressBar progress={ 50 } className="h-2" />
-							<div className="flex justify-between items-center">
-								<Text size="xs" className="text-text-tertiary">
-									Importing SEO page titles......
-								</Text>
-								<Text size="xs" className="text-text-primary">
-									50%
-								</Text>
-							</div>
+							<Text className="text-field-input text-xs font-normal">
+								50%
+							</Text>
 						</div>
 					</div>
 				</div>
-				<div className="flex justify-end items-center gap-3 mt-6">
+				<hr className="w-full border-b-0 border-x-0 border-t border-solid border-t-border-subtle p-2" />
+				<div className="flex justify-end items-center gap-3 p-2">
 					<Button variant="ghost" className="text-text-tertiary opacity-70" disabled>
 						Skip
 					</Button>
